d59b7b272c0b6e8312ca58f22a549d120eedc5b3
Dustin Wright <nzeroxpd@gmail.com>
2020-05-22 16:07:57 -0500

    Implented CommandLineRunner, added the annotation Componenet, implemented the run method.

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,4 +1,17 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-public class BootStrapData {
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by DW 05/22/20
+ */
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
