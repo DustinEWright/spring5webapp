5858e11b35abe5d9dcb6f24ec5256180ef07eb05
Dustin Wright <nzeroxpd@gmail.com>
2020-05-24 17:24:10 -0500

    Added remaining compenents to Publisher class, PublisherRepository interface, and BootStrap class.  It appears to run correctly.

--- src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Publisher.java
@@ -24,8 +24,7 @@ public class Publisher {
     public Publisher() {
     }
 
-    public Publisher(Long id, String name, String address_line_1, String city, String state, String zip) {
-        this.id = id;
+    public Publisher(String name, String address_line_1, String city, String state, String zip) {
         this.name = name;
         this.address_line_1 = address_line_1;
         this.city = city;
@@ -33,10 +32,58 @@ public class Publisher {
         this.zip = zip;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress_line_1() {
+        return address_line_1;
+    }
+
+    public void setAddress_line_1(String address_line_1) {
+        this.address_line_1 = address_line_1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
     @Override
     public String toString() {
-        return "Name{" +
-                "id=" + id +
+        return "Name{" + name +
+                ",id=" + id +
                 ", address line 1+" + address_line_1 + '\'' +
                 ", city+" + city + '\'' +
                 ", state+" + state + '\'' +
